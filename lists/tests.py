6bb5f75f66e8ad1484709cf0df9bf5a0844d0b10
Michael Jäger <M@J>
2018-05-05 21:56:33 +0200

    First unit test and url mapping, dummy view
    lists/tests.py
      * Exchange Smoke test with test that checks for unittest for resovling
        the root route ("/").
    
    lists/views.py
      * Make variable 'home_page' a function.
    
    superlists/urls.py
      * Add url entry for root route.

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,11 @@
 from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page
 
-# Create your tests here.
 
-class SmokeTest(TestCase):
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+class HomePageTest(TestCase):
+    def test_root_url_resolves_to_home_page_view(self):
+        # does the 'resolve' function find a 'view function', when "/" is called
+        found = resolve("/")
+        # the 'view function' expected to be found is called 'home_page'
+        self.assertEqual(found.func, home_page)
