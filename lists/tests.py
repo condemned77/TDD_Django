6434c58f7eeba5a3a0484c49a332592432d06e6e
Michael Jäger <M@J>
2018-05-05 22:18:51 +0200

    Basic view now return minimal HTML
    lists/tests.py
      * Add unittest 'test_home_page_returns_correct_html', which tests for the
        view function to return some minimal HTML.
    
     lists/view.py
      * Let the method 'home_page' return HTML that includes a title tag.
    
    superlists/urls.py
      * Add commentary.

--- lists/tests.py
+++ lists/tests.py
@@ -1,11 +1,28 @@
 from django.test import TestCase
 from django.urls import resolve
+from django.http import HttpRequest
+
 from lists.views import home_page
 
 
 class HomePageTest(TestCase):
-    def test_root_url_resolves_to_home_page_view(self):
-        # does the 'resolve' function find a 'view function', when "/" is called
-        found = resolve("/")
-        # the 'view function' expected to be found is called 'home_page'
-        self.assertEqual(found.func, home_page)
+  def test_root_url_resolves_to_home_page_view(self):
+      # does the 'resolve' function find a 'view function', when "/" is called
+      found = resolve("/")
+      # the 'view function' expected to be found is called 'home_page'
+      self.assertEqual(found.func, home_page)
+
+  def test_home_page_returns_correct_html(self):
+    # assemble
+    request   = HttpRequest()
+
+    # act
+    response  = home_page(request)
+
+    # assert
+    received_bytes  = response.content
+    html            = received_bytes.decode('utf8')
+    self.assertTrue(html.startswith('<html>'))
+    self.assertIn('<title>To-Do lists</title>', html)
+    self.assertTrue(html.endswith('</html>'))
+
--- superlists/urls.py
+++ superlists/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from lists import views
 
 urlpatterns = [
-    #url(r'^admin/', admin.site.urls),
+    # match a request to the root url to the view function 'home_page'
     url(r'^$', views.home_page, name='home')
 ]
