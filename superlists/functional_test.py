1c24f45b6d0fcc6006b4502fb22076b3a773f076
M <J@Ms-iMac.home>
2018-05-12 19:13:23 +0200

    Use helper method for checking rows in the functional test
    superlists/functional_test.py
      * Add helper method 'check_for_row_in_list_table', which allows
        for asserting that the passed string gets found in the rows of
        the to-do list.

--- superlists/functional_test.py
+++ superlists/functional_test.py
@@ -10,8 +10,18 @@ class NewVisitorTest(unittest.TestCase):
   def setUp(self):
     self.browser = webdriver.Firefox()
 
+
   def tearDown(self):
     self.browser.quit()
+
+
+  def check_for_row_in_list_table(self, row_text):
+    table = self.browser.find_element_by_id('id_list_table')
+    rows  = table.find_elements_by_tag_name('tr')
+    self.assertIn(
+        row_text, [row.text for row in rows]
+    ),"New to-do item did not appear in table"
+
 
   def test_can_start_a_list_and_retrieve_it_later(self):
     # Edith has heard about a cool new online to-do app. She goes
@@ -44,9 +54,7 @@ class NewVisitorTest(unittest.TestCase):
 
     time.sleep(1)
 
-    table = self.browser.find_element_by_id('id_list_table')
-    rows  = table.find_elements_by_tag_name('tr')
-    self.assertIn('1: Buy peacock feathers', [row.text for row in rows]), "New to-do item did not appear in table"
+    self.check_for_row_in_list_table('1: Buy peacock feathers')
 
     # There is still a text box inviting her to add another item. She
     # enters "Use peacock feathers to make a fly" (Edith is very 
@@ -59,16 +67,8 @@ class NewVisitorTest(unittest.TestCase):
     time.sleep(1)
 
     # The page updates again, and now shows both items on her list
-    table = self.browser.find_element_by_id('id_list_table')
-    rows  = table.find_elements_by_tag_name('tr')
-    self.assertIn(
-        '1: Buy peacock feathers', [row.text for row in rows]
-        ),"New to-do item did not appear in table"
-
-    self.assertIn(
-      '2. Use peacock feathers to make a fly',
-      [row.text for row in rows]
-    )
+    self.check_for_row_in_list_table('1: Buy peacock feathers')
+    self.check_for_row_in_list_table('2. Use peacock feathers to make a fly')
 
 # There is still a text box inviting her to add another item. She
 # enters "Use peacock feathers to make a fly"  (Edith is very methodical)
