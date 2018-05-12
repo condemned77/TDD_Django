f2a84d6a1c34c88312b5caac87062dd9f2473b15
M <J@Ms-iMac.home>
2018-05-12 16:45:02 +0200

    A single to-do item can be added to the HTML template
    lists/templates/home.html
      * Add form-tag to send a POST request once the ENTER key gets hit.
      * Add csrf (cross-site request forgery) token.
      * Add variable to accept one todo item.
    
    lists/tests.py
      * Simplyfy test 'test_home_page_returns_correct_html' and rename
        it to 'test_uses_home_template'.
    
      * Add new test 'test_can_save_a_POST_request', which adds
        data to the POST request and expects that the response contains the
        newly added to-do item.
    
    lists/views.py
      * Enable the function 'home_page' to send POST
        data to the home.html template.
    
    superlists/functional_test.py
      * Simplify assertion for the first to-do
        item.
    
      * Add a second to-do item.
    
      * Add a second assertion to check
        whether two to-do items appear in the
        HTML code after the response.

--- lists/tests.py
+++ lists/tests.py
@@ -12,12 +12,12 @@ class HomePageTest(TestCase):
       # the 'view function' expected to be found is called 'home_page'
       self.assertEqual(found.func, home_page)
 
-  def test_home_page_returns_correct_html(self):
-    # assemble
-    request   = HttpRequest()
 
-    # act
-    response  = self.client.get('/')
-
-    # assert
+  def test_uses_home_template(self):
+    response = self.client.get('/')
+    self.assertTemplateUsed(response, 'home.html')
+    
+  def test_can_save_a_POST_request(self):
+    response = self.client.post('/', data={'item_text': 'A new list item'})
+    self.assertIn('A new list item', response.content.decode())
     self.assertTemplateUsed(response, 'home.html')
