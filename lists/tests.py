f1b168289c3072d992e6a3b341bddcf2b327a9a3
M <J@Ms-iMac.home>
2018-05-13 12:51:45 +0200

    Redirect after POST, and show all items in the template
    lists/templates/home.html
      * Use for loop to list multiple to-do items.
    
    lists/tests.py
      * Add unittest 'test_only_saves_items_when_necessary', which tests
        whether accessing the root page doesn't create database entries.
    
      * Change unittest 'test_can_save_a_POST_request', to the correct item is
        added to the database if a POST request gets sent to the server.
    
      * Add unittest 'test_redirects_after_POST', to test that after a POST,
        the user gets redirected to the root page. This is best practice as
        described here: https://en.wikipedia.org/wiki/Post/Redirect/Get
    
      * Add unittest 'test_displays_all_list_item', which test whether mutliple
        to-do items are added to the database.
    
    lists/views.py
      * Implement redirect after a POST request, as described on Wikipedia:
        https://en.wikipedia.org/wiki/Post/Redirect/Get
    
      * Make sure that accessing the root page via GET request lists the
        database entries.

--- lists/tests.py
+++ lists/tests.py
@@ -1,29 +1,52 @@
 from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
+from lists.models import Item
 
 from lists.views import home_page
 
 
 class HomePageTest(TestCase):
   def test_root_url_resolves_to_home_page_view(self):
-      # does the 'resolve' function find a 'view function', when "/" is called
-      found = resolve("/")
-      # the 'view function' expected to be found is called 'home_page'
-      self.assertEqual(found.func, home_page)
+    # does the 'resolve' function find a 'view function', when "/" is called
+    found = resolve("/")
+    # the 'view function' expected to be found is called 'home_page'
+    self.assertEqual(found.func, home_page)
+
+  def test_only_saves_items_when_necessary(self):
+    self.client.get('/')
+    self.assertEqual(Item.objects.count(), 0)
 
 
   def test_uses_home_template(self):
     response = self.client.get('/')
     self.assertTemplateUsed(response, 'home.html')
+
     
   def test_can_save_a_POST_request(self):
     response = self.client.post('/', data={'item_text': 'A new list item'})
-    self.assertIn('A new list item', response.content.decode())
-    self.assertTemplateUsed(response, 'home.html')
+
+    self.assertEqual(Item.objects.count(), 1)
+    new_item = Item.objects.first()
+    self.assertEqual(new_item.text, 'A new list item')
 
 
-from lists.models import Item
+
+  def test_redirects_after_POST(self):
+    response = self.client.post('/', data={'item_text': 'A new list item'})
+    
+    self.assertEqual(response.status_code, 302)
+    self.assertEqual(response['location'], '/')
+
+
+  def test_displays_all_list_item(self):
+    Item.objects.create(text='itemy 1')
+    Item.objects.create(text='itemy 2')
+    
+    response = self.client.get('/')
+
+    self.assertIn('itemy 1', response.content.decode())
+    self.assertIn('itemy 2', response.content.decode())
 
 class ItemModelTest(TestCase):
   def test_saving_and_retrieving_items(self):
@@ -42,3 +65,5 @@ class ItemModelTest(TestCase):
     second_saved_item = saved_items[1]
     self.assertEqual(first_saved_item.text,  'Text first (ever) list item')
     self.assertEqual(second_saved_item.text, 'Second list item')
+
+
