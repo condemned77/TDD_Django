1031d2bfead31aa23e3904d1d722e792a53ca526
M <J@Ms-iMac.home>
2018-05-28 20:31:34 +0200

    [func-test] Fix execution errors
    * Move constant 'MAX_WAIT' outside of class.
    * import WebDriverException.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,10 +3,12 @@ from django.test import LiveServerTestCase
 # selenium helps us to examine web pages
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import unittest
 import time
 
 
+MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
   def setUp(self):
@@ -16,7 +18,6 @@ class NewVisitorTest(LiveServerTestCase):
   def tearDown(self):
     self.browser.quit()
 
-  MAX_WAIT = 10
 
   def wait_for_row_in_list_table(self, row_text):
     start_time = time.time()
@@ -47,19 +48,19 @@ class NewVisitorTest(LiveServerTestCase):
     header_text = self.browser.find_element_by_tag_name('h1').text
     self.assertIn('To-Do', header_text)
 
-# She is invited to enter a to-do item straight away
+    # She is invited to enter a to-do item straight away
     inputbox = self.browser.find_element_by_id('id_new_item')
     self.assertEqual(
       inputbox.get_attribute('placeholder'),
       'Enter a to-do item'
     )
 
-# She types "Buy peacock feathers" into a text box (Edith's hobby
-# is tying fly-fishing lures)
+    # She types "Buy peacock feathers" into a text box (Edith's hobby
+    # is tying fly-fishing lures)
     inputbox.send_keys('Buy peacock feathers')
 
-# When she hits enter, the page updates, and now the page lists
-# "1. Buy peacock feathers" as an item on a to-do list
+    # When she hits enter, the page updates, and now the page lists
+    # "1. Buy peacock feathers" as an item on a to-do list
     inputbox.send_keys(Keys.ENTER)
 
     self.wait_for_row_in_list_table('1: Buy peacock feathers')
@@ -76,8 +77,8 @@ class NewVisitorTest(LiveServerTestCase):
     self.wait_for_row_in_list_table('1: Buy peacock feathers')
     self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
-# There is still a text box inviting her to add another item. She
-# enters "Use peacock feathers to make a fly"  (Edith is very methodical)
+    # There is still a text box inviting her to add another item. She
+    # enters "Use peacock feathers to make a fly"  (Edith is very methodical)
     self.fail('Finish the test!')
 
 # Edith wonders whether the site will remember her list. Then she sees
